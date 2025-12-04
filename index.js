9a6c4a630ba3ad19eaecf320a4836fac74f8ab07
Luiz Fernando <luizfranca35mc@gmail.com>
2025-12-04 20:35:31 -0300

    Criei 3 rotas do API
    Listando arrays por ID (listando pedidos por ID)
    Todas usando GET,
    [get] /listarpedidos
    [get] /listarumpedido/id
    [get] /
    
    Isso cria varias rotas para uma determinada pagina,

--- index.js
+++ index.js
@@ -2,7 +2,54 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req, res){
-    res.send("funciona")
+    res.send("Esta on")
+})
+
+let listaDePedidos = [
+    {
+        "id":1,
+        "cliente" : "Guto",
+        "id_cliente":100,
+        "produto": "X-bacon",
+        "valor": 29.9
+    },
+
+    {
+        "id":2,
+        "cliente" : "Guto",
+        "id_cliente":100,
+        "produto": "X-salada",
+        "valor": 25.9
+    },
+
+    {
+        "id":3,
+        "cliente" : "Keila",
+        "id_cliente":"85",
+        "produto": "X-fominha",
+        "valor": 39.9
+    }
+    
+
+
+]
+
+//Listar
+app.get("/listarpedidos", function(req, res){
+    res.send(listaDePedidos)
+})
+
+//listar um
+app.get("/listarumpedido/:id",function(req, res) {
+    let idinformado = req.params.id
+    if(idinformado > listaDePedidos.length){
+        res.send("pedido Invalido")
+    }else{
+        res.send(listaDePedidos [idinformado-1])
+    }
+   
+
+    
 })
 
 app.listen(3000)
